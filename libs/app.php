aa806ff14956da4c03ff902cab8301a441706172
ElizabethSlop <62410186+EliSlop310@users.noreply.github.com>
2021-02-26 20:27:38 -0600

    Update app.php

--- libs/app.php
+++ libs/app.php
@@ -2,6 +2,13 @@
 class App{
 function __construct(){
   echo "<p>Nueva App</p>"
+  
+   $url=$_GET['url'];
+   $url= rtrim($url,'/');
+   $url= explode('/',$url);
+  
+  var_dump($url);
+   
 }
 
 }                                    
