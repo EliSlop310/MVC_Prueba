8688c41ebf1179c5cc5d9ecaaee0f12f93eda553
ElizabethSlop <62410186+EliSlop310@users.noreply.github.com>
2021-02-27 04:28:38 -0600

    Update errores.php

--- controllers/errores.php
+++ controllers/errores.php
@@ -4,6 +4,7 @@ class Errores extends Controller{
   
   function __construct(){
   parent::__construct();
+  $this->view->mensaje="¡¡¡Oh no!!!, Pagina no encontrada, Error de carga :(";  
   $this->view->render('errores/index');  
   //echor "<p> Error al cargar recurso </p>"
   }
