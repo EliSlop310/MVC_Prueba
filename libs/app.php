794203add655d5d9b0f54e6b53310191b494ef72
ElizabethSlop <62410186+EliSlop310@users.noreply.github.com>
2021-02-27 02:57:11 -0600

    Update app.php

--- libs/app.php
+++ libs/app.php
@@ -4,7 +4,7 @@ require_once 'controllers/errores.php';
 
 class App{
 function __construct(){
-  echo "<p>Nueva App</p>"
+  //echo "<p>Nueva App</p>"
   
    $url=$_GET['url'];
    $url= rtrim($url,'/');
